735d99df11fc1f16fc95e4c27d7608b20eeef6c6
jbueza <jbueza@gmail.com>
2010-11-25 01:53:00 -0800

    Test this out bovan

---
+++ webroot/js/mylibs/helpers/Array.js
@@ -0,0 +1,21 @@
+// Array Remove - By John Resig (MIT Licensed)
+// Makes it easier to remove items from an array
+// =================================================
+Array.prototype.remove = function(from, to) {
+    var rest = this.slice((to || from) + 1 || this.length);
+    this.length = from < 0 ? this.length + from : from;
+    return this.push.apply(this, rest);
+};
+
+// IE8 fix for indexOf
+// =================================================
+if(!Array.indexOf){
+    Array.prototype.indexOf = function(obj){
+        for(var i=0; i<this.length; i++){
+            if(this[i]==obj){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
